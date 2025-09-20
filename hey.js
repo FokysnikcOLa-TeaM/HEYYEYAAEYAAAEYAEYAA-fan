879c5fd82289d8023c349b673ffd4b4be4b92b7a
FokysnikcOLa-TeaM <polopc555@gmail.com>
2025-09-20 20:14:26 +0300

    Add files via upload

---
+++ hey.js
@@ -0,0 +1,16 @@
+// Плавная прокрутка для якорных ссылок
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        if (targetId === '#') return;
+
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
